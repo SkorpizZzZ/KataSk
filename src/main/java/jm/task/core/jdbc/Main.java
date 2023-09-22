5c922f2fbae73da6fbb617a9cf11b611b6f1d9c7
Ilgiz <Skorpizzzz@gmail.com>
2023-09-22 14:13:44 +0800

    add task 1.1.5

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,24 +1,28 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        UserService userService = new UserServiceImpl();
 
+        UserService userService = new UserServiceImpl();
+        userService.dropUsersTable();
         userService.createUsersTable();
 
         userService.saveUser("Name1", "LastName1", (byte) 20);
         userService.saveUser("Name2", "LastName2", (byte) 25);
         userService.saveUser("Name3", "LastName3", (byte) 31);
         userService.saveUser("Name4", "LastName4", (byte) 38);
-
         userService.removeUserById(1);
-        userService.getAllUsers();
+        List<User> list = userService.getAllUsers();
+        System.out.println(list);
         userService.cleanUsersTable();
         userService.dropUsersTable();
+
     }
 }
